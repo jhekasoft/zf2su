ad85c6aa994167b597b0da18d961eae14a5fed83
jhekasoft <jheka@mail.ru>
2013-05-27 02:09:26 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+zf2su
+=====
+
+zf2.su — site with Zend Framework 2 screencasts
+
---
+++ public_html/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Zend Framework 2 скринкасты на русском</title>
+    <meta name="description" content="Zend Framework 2 скринкасты на русском языке">
+    <meta name="keywords" content="zend framework 2, zf2, zf, screencast, zf2 screencast, zend framework 2 screencast, zend framework 2 скринкаст, zf2 русский, zf2 russian, zend framework 2 русский">
+    <style type="text/css">
+        body {font-size: 14px;}
+    </style>
+</head>
+
+<body>
+<div style="width: 1000px; margin: 0 auto;">
+    <h1>Zend Framework 2 скринкасты</h1>
+    <p>Добро пожаловать на сайт посвящённый Zend Framework 2! Вышел первый скринкаст.</p>
+    <p>Контактный email: info@zf2.su</p>
+
+    <h2>№1. Поднимаем Skeleton Application</h2>
+    <div>
+    </div>
+    <div>
+        <div id="disqus_thread"></div>
+        <script type="text/javascript">
+            /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+            var disqus_shortname = 'zf2su'; // required: replace example with your forum shortname
+
+            /* * * DON'T EDIT BELOW THIS LINE * * */
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+    </div>
+</div>
+</body>
+</html>
+
