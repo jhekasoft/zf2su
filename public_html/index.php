e5b4c399a6b831cea0c1c10c997145f0517cdaf4
jhekasoft <jheka@mail.ru>
2013-05-27 03:07:09 +0300

    Added video #1 and google analytics

--- public_html/index.php
+++ public_html/index.php
@@ -8,16 +8,30 @@
     <style type="text/css">
         body {font-size: 14px;}
     </style>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-41238047-1', 'zf2.su');
+      ga('send', 'pageview');
+
+    </script>
 </head>
 
 <body>
 <div style="width: 1000px; margin: 0 auto;">
     <h1>Zend Framework 2 скринкасты</h1>
-    <p>Добро пожаловать на сайт посвящённый Zend Framework 2! Вышел первый скринкаст.</p>
+    <p>Добро пожаловать на сайт посвящённый Zend Framework 2 (PHP-фреймворк)! Вышел первый скринкаст.</p>
     <p>Контактный email: info@zf2.su</p>
 
     <h2>№1. Поднимаем Skeleton Application</h2>
     <div>
+        <iframe width="1000" height="563" src="http://www.youtube.com/embed/FKD3VcFF6-0" frameborder="0" allowfullscreen></iframe>
+
+        <p>В этом видео поднимем базовый каркас на Zend Framework 2 и добавим новую страницу.
+        Исходный код по скринкасту: <a href="https://github.com/jhekasoft/zf2su">https://github.com/jhekasoft/zf2su</a>.</p>
     </div>
     <div>
         <div id="disqus_thread"></div>
